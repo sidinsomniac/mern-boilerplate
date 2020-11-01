298fccf3154a37cdd0c877ebe20e04b172a2df44
sidinsomniac <chatterjeesid1993@gmail.com>
2020-11-01 17:32:00 +0530

    Add client side of code

---
+++ client/helloworld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const HelloWorld = () => {
+    return <h1>Hello World</h1>
+}
+
+export default hot(module)(HelloWorld);
---
+++ client/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import render from 'react-dom'
+import Helloworld from './helloworld';
+
+render(Helloworld, document.getElementById("root"));
